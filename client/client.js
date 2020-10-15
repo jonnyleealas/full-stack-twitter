63235de2cb39317a04849fc4e418524e1d7e90b4
jonathon lee <jonnyleealas@gmail.com>
2020-10-15 14:31:21 -0700

    serverwroking

--- client/client.js
+++ client/client.js
@@ -3,7 +3,7 @@
 // any time you see document mean client side
 // this listen when user clicks button to grab data from form
 const form = document.querySelector('form');
-
+const API_URL = 'http://localhost:5000/mews'
 // hides loading when not loading
 const loadingElement = document.querySelector('.loading')
 loadingElement.style.display = 'none'
@@ -24,4 +24,18 @@ form.addEventListener('submit', (event)=>{
     //this give direction to hide form at send and show loading gif
     form.style.display = 'none';
     loadingElement.style.display = '';
+    // this creates a post request
+    fetch(API_URL, {
+        method: 'POST',
+        body: JSON.stringify(mew),
+        headers: {
+          'content-type': 'application/json'
+        }
+      }).then(response =>response.json())
+        .then(createdMew =>{
+            form.reset()
+            form.style.display = '';
+            loadingElement.style.display = 'none';
+          console.log(createdMew);
+      } )
 })
--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
 'use strict';
 const express = require('express')
 const app = express();
+const cors = require('cors')
+const monk = require('monk')
+// connect mongo db to online local machine called mewoer
+const db = monk('localhost/meower')
+// creates a collection inside our database
+const mews = db.get('mews')
+
+// any request made from server goes through cors and get headers from cors
+app.use(cors());
+//body parser middleware. incoming req on content type app json
+app.use(express.json());
+
 
 // checks that '/ rout works in localhost
 app.get('/', (req, res)=>{
@@ -8,6 +20,38 @@ app.get('/', (req, res)=>{
         message: 'was up'
     })
 })
-app.listen(4000,()=>{
-    console.log('listening on 4000')
+function isValidMew(mew){
+    
+    // validate whether content and name are sent through req
+    return mew.name && mew.name.toString().trim() !== '' &&
+    mew.content && mew.content.toString().trim() !== ''; 
+}
+
+app.post('/mews', (req, res)=>{
+    // stop people from hitting submit without typing a message
+    // we validate to make sure bad stuff doesn't get into the db
+    // creates the obj
+    if(isValidMew(req.body)){
+        const mew = {
+        name: req.body.name.toString(),
+        content: req.body.content.toString(),    
+        created: new Date()
+    };
+    // inserts the collection 
+    // then responds with the data that was inserted
+    mews
+    .insert(mew)
+    .then(createdMew =>{
+        res.json(createdMew)
+    })
+    } else {
+        res.status(422);
+        res.json({
+            message: 'HEY! Name and content required please'
+        })
+    }
+})
+
+app.listen(5000,()=>{
+    console.log('listening on 5000')
 })
